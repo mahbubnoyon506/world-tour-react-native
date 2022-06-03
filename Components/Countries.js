49ad16b107b89c8f2fccea924a543a3853901507
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-06-03 18:39:18 +0600

    lets have fun with react native

---
+++ Components/Countries.js
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { ScrollView } from 'react-native';
+import Country from './Country';
+
+
+export default function Countries() {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setCountries(data)
+            })
+    }, [])
+    return (
+        <View style={{marginVertical: '20px'}}>
+            <Text>Total Countries {countries.length}</Text>
+            <ScrollView>
+                {
+                    countries.map(country => <Country key={country.id} country={country}></Country>)
+                }
+            </ScrollView>
+        </View>
+    )
+}
